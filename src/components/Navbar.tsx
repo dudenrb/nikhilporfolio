1a39fd2213b2e6e6388b1bdd8b85014a43482908
Nikhil Raj <94313877+dudenrb@users.noreply.github.com>
2025-06-17 01:55:46 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,20 +15,24 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "fixed top-10 inset-x-0 z-50",
-        "w-full px-4",
+        "fixed top-4 inset-x-0 z-50",
+        "w-full px-2 sm:px-4",
         "sm:max-w-2xl sm:mx-auto",
         className
       )}
     >
-      <Menu className="text-sm sm:text-base">
-        <MenuItem item="Home" path="/" />
-        <MenuItem item="About" path="/about#about" />
-        <MenuItem item="Education" path="/about#education" />
-        <MenuItem item="Experience" path="/about#experience" />
-        <MenuItem item="Projects" path="/projects#projects" />
-        <MenuItem item="Resume" path="/resume" />
-      </Menu>
+      <div className="w-full overflow-x-auto">
+        <div className="flex flex-wrap sm:flex-nowrap items-center justify-center gap-2 sm:gap-4 py-2">
+          <Menu>
+            <MenuItem item="Home" path="/" />
+            <MenuItem item="About" path="/about#about" />
+            <MenuItem item="Education" path="/about#education" />
+            <MenuItem item="Experience" path="/about#experience" />
+            <MenuItem item="Projects" path="/projects#projects" />
+            <MenuItem item="Resume" path="/resume" />
+          </Menu>
+        </div>
+      </div>
     </div>
   );
 }
