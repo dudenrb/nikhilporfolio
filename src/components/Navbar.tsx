0076c5571a32ebd239d4a5a7faf5c1b9d3818f66
Nikhil Raj <94313877+dudenrb@users.noreply.github.com>
2025-06-17 02:19:25 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Menu, MenuItem } from "@/components/ui/navbar-menu";
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 
 export function NavbarHome() {
   return (
@@ -14,17 +14,29 @@ export function NavbarHome() {
 function Navbar({ className }: { className?: string }) {
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn(
+        "fixed top-4 inset-x-0 z-50",
+        "w-full px-2 sm:px-4",
+        "sm:max-w-3xl sm:mx-auto",
+        className
+      )}
     >
-      <Menu>
-        <MenuItem item="Home" path="/" />
-        <MenuItem item="About" path="/about#about" />
-        <MenuItem item="Education" path="/about#education" />
-        <MenuItem item="Experience" path="/about#experience" />
-        <MenuItem item="Projects" path="/projects#projects" />
-        <MenuItem item="Resume" path="/resume" />
-      </Menu>
+      {/* Add horizontal scroll container */}
+      <div className="overflow-x-auto scrollbar-none">
+        {/* Flex container for menu items */}
+        <div className="flex items-center justify-start sm:justify-center gap-4 py-2 whitespace-nowrap">
+          <Menu>
+            <MenuItem item="Home" path="/" />
+            <MenuItem item="About" path="/about#about" />
+            <MenuItem item="Education" path="/about#education" />
+            <MenuItem item="Experience" path="/about#experience" />
+            <MenuItem item="Projects" path="/projects#projects" />
+            <MenuItem item="Resume" path="/resume" />
+          </Menu>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default NavbarHome;
